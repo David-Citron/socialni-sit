268d9253e1aeb2605aaabd3999e52093a4ff1aca
David Žižlavský <davidcitron2@seznam.cz>
2024-03-23 15:20:28 +0100

    Přidání rout a metod pro zobrazení registrovacího a přihlašovacího formuláře

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,8 +4,18 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index(): string
+    public function index()
     {
         return view('welcome_message');
+    }
+
+    public function showSignInForm()
+    {
+        return view('login.php');
+    }
+    
+    public function showSignUpForm()
+    {
+        return view('register.php');
     }
 }
