43ef499eae5470ca26a53731a1d8398dcca09132
David Žižlavský <davidcitron2@seznam.cz>
2024-03-27 15:05:15 +0100

    Do routy domovské stránky přidán autentifikační filtr

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,7 +5,7 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', 'Home::index', ['filter' => 'auth']);
 $routes->get('login', 'Home::showSignInForm');
 $routes->get('register', 'Home::showSignUpForm');
 $routes->post('register', 'Auth::register');
