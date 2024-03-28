a41afabf1e521a2b144088052f308301cf10c16d
martinrumisek <mrumisek@seznam.cz>
2024-03-28 13:45:06 +0100

    Vytvořena hrubá struktura pro hlavní stránku příspěvků. (musí být upraveno: obrázky pro příspěvky "responzivnost", text pro příspěvky a komentáře.

---
+++ app/Views/mainPage.php
@@ -0,0 +1,203 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/5b42e6ef0e.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        <title>Příspěvky</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg bg-white navbar-white sticky-top" style="box-shadow: 0 4px 6px rgba(0, 0, 0, 0.4)">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            <img src="<?= base_url('assets/img/avatar.png')?>" alt="Avatar Logo" style="width: 60px;" class="rounded-pill"> 
+          </a>
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="#"><i class="fa-solid fa-plus h3 my-auto" style="color: black;"></i></a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <div class="container-fluid  mt-4">
+        <div class="row">
+            <div class="col-12 col-lg-6 mt-2">
+                <div class="container">
+                    <div class="card" style="width: 100%; border: none;">
+                        <div class="container d-flex justify-content-between p-2">
+                            <div class="d-flex">
+                                <img src="<?= base_url('assets/img/avatar.png')?>" alt="Avatar Logo" style="width:50px; height: auto;" class="rounded-pill">
+                                <h4 class="my-auto m-3">Uživatelské jméno</h4>
+                            </div>
+                            <div class="dropdown"><a class="btn" data-bs-toggle="dropdown"><i class="fa-solid fa-ellipsis-vertical h2 my-auto" style="color: black;"></i></a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Upravit</a></li>
+                                    <li><a class="dropdown-item" href="#">Smazat</a></li>
+                                  </ul>
+                            </div>
+                        </div>
+                        <div>
+                            <img src="<?= base_url('assets/img/prispevek.png')?>" class="card-img-top img-fluid" style="width: 100%" height="auto" alt="...">
+                        </div>
+                        <div class="card-body" style="box-shadow: inset 0 4px 6px rgba(0, 0, 0, 0.1)">
+                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                        <div class="d-flex justify-content-between p-2">
+                            <div class="d-flex">
+                                <i class="fa-regular fa-thumbs-up h2"></i>
+                                <i style="margin-left: 20px;" class="fa-regular fa-thumbs-down h2"></i>
+                            </div>
+                            <div>
+                                <i class="fa-regular fa-message btn btn-lg" data-bs-toggle="modal" data-bs-target="#comments0"></i>
+                            </div>
+                            <div class="modal fade" id="comments0"> <!-- ID bude vždy "comments + id-příspěvku" !-->
+                                <div class="modal-dialog modal-xl">
+                                  <div class="modal-content">
+                                    <div class="modal-header">
+                                      <h4 class="modal-title">Komentáře</h4>
+                                      <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <div class="container-fluid row">
+                                            <div class="col-12 col-lg-3">
+                                                <div class="d-flex">
+                                                    <img src="<?= base_url('assets/img/avatar.png')?>" alt="Avatar Logo" style="width:50px; height: auto; " class=" rounded-pill">
+                                                    <p class="small my-auto" style="margin-left: 3px;">Uživatelské jméno</p>
+                                                </div>
+                                            </div>
+                                            <div class="col-12 col-lg-9"><p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean id metus id velit ullamcorper pulvinar. Nulla pulvinar eleifend sem. Curabitur sagittis hendrerit ante. Suspendisse sagittis ultrices augue. Sed convallis magna eu sem. Maecenas libero. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Fusce aliquam vestibulum ipsum. Maecenas libero. Sed elit dui, pellentesque a, faucibus vel, interdum nec, diam. Etiam commodo dui eget wisi. Vivamus porttitor turpis ac leo. Phasellus enim erat, vestibulum vel, aliquam a, posuere eu, velit. Aliquam ante. Pellentesque arcu. Praesent dapibus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque ipsum.</p></div>
+                                            <div class="col-12 col-lg-3">
+                                                <div class="d-flex">
+                                                    <img src="<?= base_url('assets/img/avatar.png')?>" alt="Avatar Logo" style="width:50px; height: auto; " class=" rounded-pill">
+                                                    <p class="small my-auto" style="margin-left: 3px;">Uživatelské jméno</p>
+                                                </div>
+                                            </div>
+                                            <div class="col-12 col-lg-9"><p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean id metus id velit ullamcorper pulvinar. Nulla pulvinar eleifend sem. Curabitur sagittis hendrerit ante. Suspendisse sagittis ultrices augue. Sed convallis magna eu sem. Maecenas libero. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Fusce aliquam vestibulum ipsum. Maecenas libero. Sed elit dui, pellentesque a, faucibus vel, interdum nec, diam. Etiam commodo dui eget wisi. Vivamus porttitor turpis ac leo. Phasellus enim erat, vestibulum vel, aliquam a, posuere eu, velit. Aliquam ante. Pellentesque arcu. Praesent dapibus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque ipsum.</p></div>
+                                        </div>
+                                    </div>
+                                    <div class="container">
+                                        <form action="" method="post">
+                                            <div class="form-floating">
+                                                <textarea name="comments" class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px; resize:none;"></textarea>
+                                                <label for="floatingTextarea2">Komentář</label>
+                                              </div>
+                                              <button class="btn btn-white shadow-lg m-3" type="submit"> Přidat</button>
+                                        </form>
+                                    </div>
+                              
+                                  </div>
+                                </div>
+                              </div>
+                        </div>
+                        <div class=" d-flex justify-content-between p-3" style="background-color: #F5F5F5; box-shadow: 0 4px 6px rgba(0, 0, 0, 0.3)">
+                            <div class="d-flex">
+                                <div class="d-flex my-auto">
+                                    <i class="fa-regular fa-thumbs-up my-auto"></i>
+                                    <p class="my-auto" style="margin-left: 2px;">131 210</p>
+                                </div>
+                                <div class="d-flex my-auto" style="margin-left: 20px;">
+                                    <i class="fa-regular fa-thumbs-down my-auto"></i>
+                                    <p class="my-auto" style="margin-left: 2px;">131 210</p>
+                                </div>
+                            </div>
+                            <div class="d-flex">
+                                    <p class="my-auto">131 210</p>
+                                    <i class="fa-regular fa-message my-auto" style="margin-left: 2px;"></i>
+                            </div>
+                        </div>
+                      </div>
+                </div>
+            </div>
+            <div class="col-12 col-lg-6 mt-2">
+                <div class="container">
+                    <div class="card" style="width: 100%; border: none;">
+                        <div class="container d-flex justify-content-between p-2">
+                            <div class="d-flex">
+                                <img src="<?= base_url('assets/img/avatar.png')?>" alt="Avatar Logo" style="width:50px; height: auto;" class="rounded-pill">
+                                <h4 class="my-auto m-3">Uživatelské jméno</h4>
+                            </div>
+                            <div class="dropdown"><a class="btn" data-bs-toggle="dropdown"><i class="fa-solid fa-ellipsis-vertical h2 my-auto" style="color: black;"></i></a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Upravit</a></li>
+                                    <li><a class="dropdown-item" href="#">Smazat</a></li>
+                                  </ul>
+                            </div>
+                        </div>
+                        <div>
+                            <img src="<?= base_url('assets/img/prispevek.png')?>" class="card-img-top img-fluid" style="width: 100%" height="auto" alt="...">
+                        </div>
+                        <div class="card-body" style="box-shadow: inset 0 4px 6px rgba(0, 0, 0, 0.1)">
+                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                        <div class="d-flex justify-content-between p-2">
+                            <div class="d-flex">
+                                <i class="fa-regular fa-thumbs-up h2"></i>
+                                <i style="margin-left: 20px;" class="fa-regular fa-thumbs-down h2"></i>
+                            </div>
+                            <div>
+                                <i class="fa-regular fa-message btn btn-lg" data-bs-toggle="modal" data-bs-target="#comments1"></i>
+                            </div>
+                            <div class="modal fade" id="comments1"> <!-- ID bude vždy "comments + id-příspěvku" !-->
+                                <div class="modal-dialog modal-xl">
+                                  <div class="modal-content">
+                                    <div class="modal-header">
+                                      <h4 class="modal-title">Komentáře</h4>
+                                      <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <div class="container-fluid row">
+                                            <div class="col-12 col-lg-3">
+                                                <div class="d-flex">
+                                                    <img src="<?= base_url('assets/img/avatar.png')?>" alt="Avatar Logo" style="width:50px; height: auto; " class=" rounded-pill">
+                                                    <p class="small my-auto" style="margin-left: 3px;">Uživatelské jméno</p>
+                                                </div>
+                                            </div>
+                                            <div class="col-12 col-lg-9"><p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean id metus id velit ullamcorper pulvinar. Nulla pulvinar eleifend sem. Curabitur sagittis hendrerit ante. Suspendisse sagittis ultrices augue. Sed convallis magna eu sem. Maecenas libero. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Fusce aliquam vestibulum ipsum. Maecenas libero. Sed elit dui, pellentesque a, faucibus vel, interdum nec, diam. Etiam commodo dui eget wisi. Vivamus porttitor turpis ac leo. Phasellus enim erat, vestibulum vel, aliquam a, posuere eu, velit. Aliquam ante. Pellentesque arcu. Praesent dapibus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque ipsum.</p></div>
+                                            <div class="col-12 col-lg-3">
+                                                <div class="d-flex">
+                                                    <img src="<?= base_url('assets/img/avatar.png')?>" alt="Avatar Logo" style="width:50px; height: auto; " class=" rounded-pill">
+                                                    <p class="small my-auto" style="margin-left: 3px;">Uživatelské jméno</p>
+                                                </div>
+                                            </div>
+                                            <div class="col-12 col-lg-9"><p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean id metus id velit ullamcorper pulvinar. Nulla pulvinar eleifend sem. Curabitur sagittis hendrerit ante. Suspendisse sagittis ultrices augue. Sed convallis magna eu sem. Maecenas libero. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Fusce aliquam vestibulum ipsum. Maecenas libero. Sed elit dui, pellentesque a, faucibus vel, interdum nec, diam. Etiam commodo dui eget wisi. Vivamus porttitor turpis ac leo. Phasellus enim erat, vestibulum vel, aliquam a, posuere eu, velit. Aliquam ante. Pellentesque arcu. Praesent dapibus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque ipsum.</p></div>
+                                        </div>
+                                    </div>
+                                    <div class="container">
+                                        <form action="" method="post">
+                                            <div class="form-floating">
+                                                <textarea name="comments" class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px; resize:none;"></textarea>
+                                                <label for="floatingTextarea2">Komentář</label>
+                                              </div>
+                                              <button class="btn btn-white shadow-lg m-3" type="submit"> Přidat</button>
+                                        </form>
+                                    </div>
+                              
+                                  </div>
+                                </div>
+                              </div>
+                        </div>
+                        <div class=" d-flex justify-content-between p-3" style="background-color: #F5F5F5; box-shadow: 0 4px 6px rgba(0, 0, 0, 0.3)">
+                            <div class="d-flex">
+                                <div class="d-flex my-auto">
+                                    <i class="fa-regular fa-thumbs-up my-auto"></i>
+                                    <p class="my-auto" style="margin-left: 2px;">131 210</p>
+                                </div>
+                                <div class="d-flex my-auto" style="margin-left: 20px;">
+                                    <i class="fa-regular fa-thumbs-down my-auto"></i>
+                                    <p class="my-auto" style="margin-left: 2px;">131 210</p>
+                                </div>
+                            </div>
+                            <div class="d-flex">
+                                    <p class="my-auto">131 210</p>
+                                    <i class="fa-regular fa-message my-auto" style="margin-left: 2px;"></i>
+                            </div>
+                        </div>
+                      </div>
+                </div>
+            </div>
+        </div>
+      </div>
+</body>
+</html>
