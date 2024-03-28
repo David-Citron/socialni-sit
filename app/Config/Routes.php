98ccd968ac63aec7e9876d340cd5b3da0471008f
David Žižlavský <davidcitron2@seznam.cz>
2024-03-28 23:31:31 +0100

    Přidání komentářů pro sekce rout

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,11 +5,14 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+
+// Routes for client side
 $routes->get('/', 'Home::index', ['filter' => 'auth']);
 $routes->get('login', 'Home::showSignInForm');
 $routes->get('register', 'Home::showSignUpForm');
 $routes->get('mainPage', 'Home::showContributions');
 
+// Routes for server side
 $routes->get('logout', 'Auth::logOut');
 $routes->post('register', 'Auth::register');
 $routes->post('login', 'Auth::login');
