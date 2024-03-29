0d73a839d8e59693c561742153dbdd1bf7bd7e5c
martinrumisek <mrumisek@seznam.cz>
2024-03-29 14:35:59 +0100

    Vytvořen Seeder pro generování dat pro uživatele. (Vygerenovaných 100 uživatelů)

---
+++ app/Database/Seeds/UserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+use App\Models\UserModel;
+
+class UserSeeder extends Seeder
+{
+    public function run()
+    {
+        $faker = \Faker\Factory::create();
+        $userModel = new UserModel();
+        for ($i = 0; $i < 100; $i++) {
+            $username = 'user' . $i;
+            $password = $username;
+            $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+            $data = [
+                'uzivatelske_jmeno' => $username,
+                'heslo' => $hashedPassword,
+                'email' => $username . '@example.com',
+                'datum_narozeni' => $faker->date,
+                'admin' => 0,
+                'obrazek' => 'avatar.png',
+                'popis' => 'Učty na základě generování',
+            ];
+
+            $userModel->insert($data);
+        }
+    }
+}
