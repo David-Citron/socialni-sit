79b2b8de3dc8777502a5de3bb5dac81208d37686
David Žižlavský <davidcitron2@seznam.cz>
2024-04-01 14:40:29 +0200

    Vytvoření controlleru Post

---
+++ app/Controllers/Post.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controllers;
+
+class Post extends BaseController
+{
+    public function create()
+    {
+        return;
+    }
+
+    public function edit($id)
+    {
+        return;
+    }
+
+    public function delete($id)
+    {
+        return;
+    }
+}
