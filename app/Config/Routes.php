88532aa58150621c9ccaa215998241c587a89273
David Žižlavský <davidcitron2@seznam.cz>
2024-04-01 14:30:54 +0200

    Přidání rout pro vytváření, editování a mazání příspěvků

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -11,9 +11,18 @@ $routes->get('/', 'Home::showMainPage', ['filter' => 'auth']);
 $routes->get('login', 'Home::showSignInForm');
 $routes->get('register', 'Home::showSignUpForm');
 $routes->get('user/(:any)', 'Home::showProfile/$1', ['filter' => 'auth']);
+$routes->group('post', ['filter' => 'auth'], static function ($routes) {
+    $routes->get('create', 'Home::showPostCreateForm');
+    $routes->get('edit', 'Home::showPostEditForm');
+});
 $routes->get('accounts', 'Home::showAccounts', ['filter' => ['auth', 'admin']]);
 
 // Routes for server side
 $routes->get('logout', 'Auth::logOut');
 $routes->post('register', 'Auth::register');
 $routes->post('login', 'Auth::login');
+$routes->group('post', ['filter' => 'auth'], static function ($routes) {
+    $routes->put('create', 'Post::create');
+    $routes->post('edit', 'Post::edit');
+    $routes->delete('(:num)', 'Post::delete/$1');
+});
