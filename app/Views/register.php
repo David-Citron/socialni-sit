b979c0f9da1e099c9db910600a626772c03f8ea2
Jakub <jkbstransky@gmail.com>
2024-04-03 16:56:45 +0200

    Upravení zavíracího tlačítka modalu v register.php

--- app/Views/register.php
+++ app/Views/register.php
@@ -58,7 +58,7 @@
 
             <!-- Modal footer -->
             <div class="modal-footer">
-                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Zavřít</button>
             </div>
 
             </div>
