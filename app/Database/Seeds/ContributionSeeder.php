57859718d5a07334890a164e04a386d512683e0e
martinrumisek <mrumisek@seznam.cz>
2024-04-12 22:00:14 +0200

    Změna seederu pro generování dat na základě změny db. (změna v like & dislike).

--- app/Database/Seeds/ContributionSeeder.php
+++ app/Database/Seeds/ContributionSeeder.php
@@ -3,29 +3,26 @@
 namespace App\Database\Seeds;
 
 use CodeIgniter\Database\Seeder;
+use Faker\Factory;
 use App\Models\PostModel;
 use App\Models\UserModel;
 class ContributionSeeder extends Seeder
 {
     public function run()
     {
-        $faker = \Faker\Factory::create('en_US');
+        $faker = Factory::create('cs_CZ');
         $userModel = new UserModel();
         $postModel = new PostModel();
 
         $userID = $userModel->findColumn('id');
         for ($i = 0; $i < 50; $i++) {
-            $text = 'Text příspěvku je generovaný na základě přidání dat v db.';
-            $countLike = 50;
-            $countDislike = 50;
-            $countComments = 50;
-            $createDate = 2024-03-29;
+            $name = $faker->realText(10);
+            $text = $faker->realText(1000);
+            $createDate = $faker->date('Y-m-d');
             $IDUser = $faker->randomElement($userID);;
             $data = [
+                'nazev' => $name,
                 'text' => $text,
-                'pocet_palcu_nahoru' => $countLike,
-                'pocet_palcu_dolu' => $countDislike,
-                'pocet_komentaru' => $countComments,
                 'pridano' => $createDate,
                 'uzivatel_id' => $IDUser,
             ];
